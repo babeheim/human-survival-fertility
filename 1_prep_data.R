f562b145d4ecbb6ab4f9b5b78a1b8649c5433478
Bret Beheim <beheim@gmail.com>
2023-04-16 21:17:35 +0100

    updated code to produce tikz slides automatically

--- 1_prep_data.R
+++ 1_prep_data.R
@@ -4,11 +4,28 @@ source("project_support.R")
 # downloaded in 2017 (url's are dead)
 
 # https://ourworldindata.org/grapher/natural-population-growth
-# https://ourworldindata.org/grapher/life-expectancy
-# https://ourworldindata.org/grapher/children-born-per-woman
+
+# Natural growth rate
+# UN population division 
+# Crude birth rate minus the crude death rate. Represents the portion of population growth (or decline) determined exclusively by births and deaths.
 
 # (for TFR, originally used https://ourworldindata.org/grapher/total-fertility-rate but link is dead
 # not used: children-per-woman-UN
+
+# https://ourworldindata.org/grapher/life-expectancy
+
+# Life expectancy at birth
+# The average number of years that a newborn could expect to live, if he or she were to pass through life exposed to the sex- and age-specific death rates prevailing at the time of his or her birth, for a specific year, in a given country, territory, or geographic area.
+# source: United Nations, Department of Economic and Social Affairs, Population Division (2022). World Population Prospects 2022, Online Edition; Zijdeman et al. (2015) (via clio-infra.eu); Riley, J. C. (2005). Estimates of Regional and Global Life Expectancy, 1800-2001. Population and Development Review, 31(3), 537–543. http://www.jstor.org/stable/3401478
+
+# https://ourworldindata.org/grapher/children-born-per-woman
+
+# Children Born per Woman
+# Fertility rate
+# Total fertility rate represents the number of children that would be born to a woman if she were to live to the end of her childbearing years and bear children in accordance with age-specific fertility rates of the specified year.
+# publisher: Gapminder
+# Data is that of version 12 of Gapminder, the latest version as of 2019. This is the full fertility rate dataset published by Gapminder.
+
 
 cat("process data\n")
 
@@ -62,11 +79,11 @@ life |>
   left_join(select(growth, gr, key), by = "key") |>
   select(-key) -> d 
 
-stopifnot(nrow(d) == 89966) # originally 13881 in 2017
-stopifnot(sum(is.na(d$gr)) == 1918) # originally 11687 in 2017
+stopifnot(nrow(d) == 16084) # originally 13881 in 2017
+stopifnot(sum(is.na(d$gr)) == 1908) # originally 11687 in 2017
 
 stopifnot(range(d$Year) == c(1543, 2021))
-stopifnot(length(unique(d$Code)) == 198)
+stopifnot(length(unique(d$Code)) == 197)
 
 write.csv(d, "data/nation_year_demographics.csv", row.names = FALSE)
 
--- 2_create_plots.R
+++ 2_create_plots.R
@@ -1,6 +1,8 @@
 
 source("project_support.R")
 
+library(tikzDevice)
+
 dir_init("./figures")
 
 # load from polygon data
@@ -8,52 +10,70 @@ foragers <- read.csv("data/forager-polygon.csv")
 pastoralists <- read.csv("data/pastoralist-polygon.csv")
 hort <- read.csv("data/horticulturalist-polygon.csv")
 
-# load country-year stats from 2017 ourworldindata
 d <- read.csv("data/nation_year_demographics.csv")
-d <- filter(d, Code != "") 
-
-d$my_col <- data_gradient(d$gr, my.start=0, my.stop=3.5)
-
-d$my_col[is.na(d$gr)] <- gray(0.8)
 
 # together these caluclate gr exactly?
-
-#  growth = a + b log(tfr) + c log(le)
+# growth = a + b log(tfr) + c log(le)
 
 d$ln_tfr <- log(d$tfr)
 d$ln_e0 <- log(d$e0)
 
 m1 <- lm(d$gr ~ d$ln_tfr + d$ln_e0) # pretty close! R^2 = 0.88
 
+growth_isocline <- function(x, r) exp( (coef(m1)[1] + coef(m1)["d$ln_e0"] * log(x) - r) / -coef(m1)["d$ln_tfr"] )
+
 d$gr_pred <- coef(m1)[1] + coef(m1)[2]*d$ln_tfr + coef(m1)[3]*d$ln_e0
 
 png("figures/growth_predicted_actual.png", res = 300, height = 6, width = 6, units = "in")
-plot(d$gr_pred, d$gr, xlab = "predicted growth", ylab = "actual growth rate", main = "gr ~ ln(tfr) + ln(e0)")
-abline(0, 1, lty = 2, col = "red")
+
+plot(d$gr_pred, d$gr, xlab = "predicted growth", ylab = "actual growth rate", main = "gr ~ ln(tfr) + ln(e0)", col = col.alpha("dodgerblue", 0.8))
+
+abline(0, 1, col = "red")
 
 text(-0.6, 3.7, paste0("R^2 = ", round(summary(m1)$r.squared, 2)))
 
-dev.off() # uh oh....
+dev.off()
 
 
-growth_isocline <- function(x, r) exp( (coef(m1)[1] + coef(m1)["d$ln_e0"] * log(x) - r) / -coef(m1)["d$ln_tfr"] )
+
+tikz("figures/production_vitals.tex", height = 5, width = 6)
 
 plot(NULL, frame.plot=FALSE, 
   ylim = c(1, 9), xlim = c(20, 85), 
   xlab = "Life expectancy at birth", 
   ylab = "Total fertility rate", las = 1, axes = FALSE,
-  main = year)
+  main = "Vital rates by production system (Gurven \\& Kaplan 2007)")
 
-polygon(hort$x, hort$y, border=NA, col=col_alpha(gray(0.8), 0.4) )
-polygon(foragers$x, foragers$y, border=NA, col=col_alpha(gray(0.8), 0.4) )
-polygon(pastoralists$x, pastoralists$y, border=NA, col=col_alpha(gray(0.8), 0.4) )
+polygon(foragers$x, foragers$y, border=NA, col=col_alpha(gray(0.8), 0.4))
+
+polygon(hort$x, hort$y, border=NA, col=col_alpha("lightblue", 0.4))
+
+polygon(pastoralists$x, pastoralists$y, border=NA, col=col_alpha("green", 0.1))
 
 for (r in 0:4) curve(growth_isocline(x, r), from = 0, to = 100, add = TRUE, lty = 2, col = gray(0.4))
 
+axis(2, at=seq(1,9, by=1), las=1, labels=FALSE)
+axis(2, at=seq(1,9, by=2), las=1)
+axis(1, at=seq(20,90, by=10), las=1)
 
-# d <- read.csv("data/nation_year_demographics.csv")
-# d <- filter(d, Code != "") 
-# lots of categories of countries here
+text(39.70611, 2.407064, label="0\\% population growth", srt=(-14.5), col = gray(0.8))
+text(43.22775, 3.401599, label="1\\%", srt=(-14.5), col = gray(0.8))
+text(48.51021, 4.622958, label="2\\%", srt=(-16), col = gray(0.8))
+text(55.67089, 6.245621, label="3\\%", srt=(-18), col = gray(0.8))
+text(64.35761, 8.321932, label="4\\%", srt=(-20), col = gray(0.8))
+
+gk <- read.csv("data/gurven-kaplan-2007/gurven-kaplan-2007-data.csv")
+
+tar <- grep("foragers", gk$category)
+points(gk$e0[tar], gk$tfr[tar], pch = 16)
+
+tar <- grep("horticulturalists", gk$category)
+points(gk$e0[tar], gk$tfr[tar], pch = 16, col = "lightblue")
+
+tar <- grep("pastoralist", gk$category)
+points(gk$e0[tar], gk$tfr[tar], pch = 16, col = "darkseagreen")
+
+dev.off()
 
 # r = a + b*ln_e0 + c*ln_tfr
 # -c*ln_tfr + r = a + b*ln_e0 
@@ -69,11 +89,10 @@ for (r in 0:4) curve(growth_isocline(x, r), from = 0, to = 100, add = TRUE, lty 
 
 
 
-dir_init("./test")
-
-# years: 1880, 1905, 1930, 1955, 1980, 2005
+dir_init("./figures/ourworldindata")
 
 years <- sort(unique(d$Year))
+years <- years[years > 1850]
 
 list(
   list(
@@ -106,8 +125,6 @@ list(
   )
 ) |> bind_rows() |> as.data.frame() -> named
 
-# it would be useful to have rolling values for years there is no data...
-
 named$tfr <- NA
 named$e0 <- NA
 
@@ -115,8 +132,8 @@ for (i in 1:length(years)) {
 
   year <- years[i]
 
-  my_filename <- paste0("test/test", year, ".png")
-  png(my_filename, res = 300, units = "in", height = 6, width = 7)
+  my_filename <- paste0("figures/ourworldindata/frame", i, ".tex")
+  tikz(my_filename, height = 5, width = 6)
 
   plot(NULL, frame.plot=FALSE, 
     ylim = c(1, 9), xlim = c(20, 85), 
@@ -124,9 +141,11 @@ for (i in 1:length(years)) {
     ylab = "Total fertility rate", las = 1, axes = FALSE,
     main = year)
 
-  polygon(hort$x, hort$y, border=NA, col=col_alpha(gray(0.8), 0.4) )
-  polygon(foragers$x, foragers$y, border=NA, col=col_alpha(gray(0.8), 0.4) )
-  polygon(pastoralists$x, pastoralists$y, border=NA, col=col_alpha(gray(0.8), 0.4) )
+  polygon(foragers$x, foragers$y, border=NA, col=col_alpha(gray(0.8), 0.4))
+
+  polygon(hort$x, hort$y, border=NA, col=col_alpha("lightblue", 0.4))
+
+  polygon(pastoralists$x, pastoralists$y, border=NA, col=col_alpha("green", 0.1))
 
   for (r in 0:4) curve(growth_isocline(x, r), from = 0, to = 100, add = TRUE, lty = 2, col = gray(0.4))
 
@@ -138,15 +157,15 @@ for (i in 1:length(years)) {
   axis(2, at=seq(1,9, by=2), las=1)
   axis(1, at=seq(20,90, by=10), las=1)
 
-  text(39.70611, 2.407064, label="0% population growth", srt=(-14.5), col = gray(0.8))
-  text(43.22775, 3.401599, label="1%", srt=(-14.5), col = gray(0.8))
-  text(48.51021, 4.622958, label="2%", srt=(-16), col = gray(0.8))
-  text(55.67089, 6.245621, label="3%", srt=(-18), col = gray(0.8))
-  text(64.35761, 8.321932, label="4%", srt=(-20), col = gray(0.8))
+  text(39.70611, 2.407064, label="0\\% population growth", srt=(-14.5), col = gray(0.8))
+  text(43.22775, 3.401599, label="1\\%", srt=(-14.5), col = gray(0.8))
+  text(48.51021, 4.622958, label="2\\%", srt=(-16), col = gray(0.8))
+  text(55.67089, 6.245621, label="3\\%", srt=(-18), col = gray(0.8))
+  text(64.35761, 8.321932, label="4\\%", srt=(-20), col = gray(0.8))
 
   d_year <- filter(d, Year == year)
   
-  points(d_year$e0, d_year$tfr, pch = 16, col = col.alpha("dodgerblue", 0.3))
+  points(d_year$e0, d_year$tfr, pch = 16, col = col_alpha("orangered", 0.4))
 
   tar <- which(named$Code %in% d_year$Code)
   link <- match(named$Code[tar], d_year$Code)
@@ -161,5 +180,3 @@ for (i in 1:length(years)) {
   cat(my_filename, "\n")
 
 }
-
-
